a17afc0edc626f7744861aac9261dc29c1c3a61d
Kadir Mutlu <kadirmutluu@gmail.com>
2016-05-15 01:47:54 +0300

    login.php eklendi

---
+++ login.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
+    <link href="css/login.css" rel="stylesheet" />
+    <title>Admin Paneli Giriş</title>
+	</head>
+    <body>
+        <div class="login-page">
+            <div class="form">
+                <form class="login-form">
+                <input type="text" placeholder="Kullanıcı Adı"/>
+                <input type="password" placeholder="Parola"/>
+                <button>Giriş</button>
+                </form>
+            </div>
+         </div>
+    </body>
+</html>
