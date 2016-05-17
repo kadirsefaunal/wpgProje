958c8027760d22189e4a11bb61674df5e62f3c5a
kadirsefaunal <kadir-unal@outlook.com>
2016-05-17 22:12:15 +0300

    session işlemi tamamlandı

--- admin.php
+++ admin.php
@@ -1,26 +1,12 @@
 <?php
     include("ayar.php");
     session_start();
-    print "admin panel";
+    print "admin panel <br />";
+    
     if(!isset($_SESSION["kID"])){
-        $kAdi = $_POST["kullaniciadi"];
-        $kParola = $_POST["kullaniciparola"];
-        $komut = $db->prepare("SELECT * FROM kullanicilar WHERE KullaniciAdi = ? AND KullaniciSifre = ?");
-        $komut->execute(array($kAdi, $kParola));
-        $sonuc = $komut->fetch(PDO::FETCH_ASSOC);
-        if ($komut->rowCount()) { 
-            
-            $_SESSION["kID"] = $sonuc["KullaniciID"];
-            $_SESSION["kAdi"] = $sonuc["KullaniciAdi"];
-            echo $_SESSION["kID"]. "<br />". $_SESSION["kAdi"];
-            print "Hoş Geldin ". $_SESSION["kAdi"];
-            print "<a href='cikis.php'>Çıkış</a>";
-        }else{
-            header("location: login.php");
-        }
+        header("location: login.php");
     }
-    else{
-        print "Hoş Geldin ". $_SESSION["kAdi"];
-        print "<a href='cikis.php'>Çıkış</a>";
+    else {
+        print $_SESSION["kID"]. "<br />". $_SESSION["kAdi"]. "<a href = 'cikis.php'>Çıkış</a>";
     }
 ?>
--- login.php
+++ login.php
@@ -1,3 +1,27 @@
+<?php
+    include("ayar.php");
+    session_start();
+    if(!isset($_SESSION["kID"])){
+        $kAdi = @$_POST["kullaniciadi"];
+        $kParola = @$_POST["kullaniciparola"];
+        $komut = $db->prepare("SELECT * FROM kullanicilar WHERE KullaniciAdi = ? AND KullaniciSifre = ?");
+        $komut->execute(array($kAdi, $kParola));
+        $sonuc = $komut->fetch(PDO::FETCH_ASSOC);
+        if ($komut->rowCount()) { 
+            $_SESSION["kID"] = $sonuc["KullaniciID"];
+            $_SESSION["kAdi"] = $sonuc["KullaniciAdi"];
+            header("location: admin.php");
+        }else{
+            if($kAdi != null && $kParola != null){
+                print "Kullanıcı adı yada parola hatalı";
+            }
+        }
+    }
+    else{
+        header("location: admin.php");
+    }
+?>
+
 <html>
     <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -8,7 +32,7 @@
     <body>
         <div class="login-page">
             <div class="form">
-                <form class="login-form" action = "admin.php" method = "POST">
+                <form class="login-form" action = "login.php" method = "POST">
                     <h3><b>Admin Paneli</b></h3>
                     <div class="cizgi"></div>
                     <input type="text" placeholder="Kullanıcı Adı" name = "kullaniciadi"/>
